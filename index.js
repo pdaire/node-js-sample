92c30414017c6d688db190052c28191081b4c4fa
Pedro Daire <pdaire@gmail.com>
2016-03-23 16:38:41 -0300

    Revert "[add] console logs for request attributes"
    This reverts commit 959b648fd7fd7ea61a3f182f6be86b0b2c3c96b3.

--- index.js
+++ index.js
@@ -4,30 +4,14 @@ var app = express()
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(req, res) {
-  console.log(req);
-  res.send('¡Hooola node!');
+app.get('/', function(request, response) {
+  console.log(request);
+  response.send('¡Hooola node!');
 })
 
-app.post('/', function(req, res) {
-  console.log(req.cookies);
-  console.log(req.fresh);
-  console.log(req.hostname);
-  console.log(req.ip);
-  console.log(req.ips);
-  console.log(req.method);
-  console.log(req.originalUrl);
-  console.log(req.params);
-  console.log(req.path);
-  console.log(req.protocol);
-  console.log(req.query);
-  console.log(req.route);
-  console.log(req.secure);
-  console.log(req.signedCookies);
-  console.log(req.stale);
-  console.log(req.subdomains);
-  console.log(req.xhr);
-  res.status(200).send();
+app.post('/', function(request, response) {
+  console.log(request);
+  response.status(200).send();
 })
 
 app.listen(app.get('port'), function() {
